a3f18a585d427c3d68d06b2cdff9ca3c584e6c03
Salome Bassey <uc.salome2@gmail.com>
2023-07-13 10:14:42 +0000

    error 3 fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,9 @@
 -- ranked by their longevity
 -- Column names must be: band_name & lifespan
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+SELECT band_name, 
+       (YEAR('2022-01-01') - CAST(SUBSTRING_INDEX(formed, '-', 1) AS UNSIGNED)) AS lifespan
+FROM bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
+
